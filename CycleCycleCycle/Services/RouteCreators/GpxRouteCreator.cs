6c17e162b77b90361489644c7f171ff47c37a370
James Randall <james@accidentalfish.com>
2011-09-11 17:49:02 +0100

    Added support for v1.0 GPX files

--- CycleCycleCycle/Services/RouteCreators/GpxRouteCreator.cs
+++ CycleCycleCycle/Services/RouteCreators/GpxRouteCreator.cs
@@ -10,6 +10,9 @@ namespace CycleCycleCycle.Services.RouteCreators
 {
     public class GpxRouteCreator : BaseRouteCreator, IRouteCreator
     {
+        private const string Gpx_1_0 = "http://www.topografix.com/GPX/1/0";
+        private const string Gpx_1_1 = "http://www.topografix.com/GPX/1/1";
+
         public GpxRouteCreator(IDistanceCalculator distanceCalculator) : base(distanceCalculator)
         {
             
@@ -19,10 +22,64 @@ namespace CycleCycleCycle.Services.RouteCreators
         {
             XmlNamespaceManager namespaces = new XmlNamespaceManager(new NameTable());
             XNamespace ns = routeDocument.Root.GetDefaultNamespace();
+            Route route = null;
+            if (ns.NamespaceName.Equals(Gpx_1_0))
+            {
+                route = CreateGpx_1_0_Route(routeDocument, ns);
+            }
+            else if (ns.NamespaceName.Equals(Gpx_1_1))
+            {
+                route = CreateGpx_1_1_Route(routeDocument, ns);
+            }
+            else
+            {
+                throw new RouteCreatorException(String.Format("Gpx namespace not recognized: {0}", ns.NamespaceName));
+            }
+            return route;
+        }
+
+        private Route CreateGpx_1_0_Route(XDocument routeDocument, XNamespace ns)
+        {
             XElement track = routeDocument.Root.Element(ns + "trk");
             string name = track.Element(ns + "name").Value;
 
+            DateTime time = DateTime.Parse(routeDocument.Root.Element(ns + "time").Value);
+
             Route newRoute = new Route { Name = name, DateCreated = DateTime.Now };
+            List<RoutePoint> routePoints = new List<RoutePoint>();
+            var trackPoints = track.Element(ns + "trkseg").Elements(ns + "trkpt");
+            foreach (XElement trackPoint in trackPoints)
+            {
+                RoutePoint routePoint = new RoutePoint();
+                routePoint.Latitude = double.Parse(trackPoint.Attribute("lat").Value);
+                routePoint.Longitude = double.Parse(trackPoint.Attribute("lon").Value);
+                routePoint.Elevation = double.Parse(trackPoint.Element(ns + "ele").Value);
+                routePoint.TimeRecorded = time;
+                routePoints.Add(routePoint);
+
+                time = time.AddSeconds(1); // purely arbitary really
+            }
+            routePoints = routePoints.OrderBy(rp => rp.TimeRecorded).ToList();
+            int index = 0;
+            foreach (RoutePoint routePoint in routePoints)
+            {
+                routePoint.SequenceIndex = index;
+                index++;
+            }
+            newRoute.RoutePoints = routePoints;
+            newRoute.Distance = CalculateDistance(newRoute.RoutePoints);
+            Tuple<double, double> ascentDescent = CalculateAscentDescent(newRoute.RoutePoints);
+            newRoute.TotalAscent += ascentDescent.Item1;
+            newRoute.TotalDescent += ascentDescent.Item2;
+            return newRoute;
+        }
+
+        private Route CreateGpx_1_1_Route(XDocument routeDocument, XNamespace ns)
+        {
+            XElement track = routeDocument.Root.Element(ns + "trk");
+            string name = track.Element(ns + "name").Value;
+
+            Route newRoute = new Route {Name = name, DateCreated = DateTime.Now};
             List<RoutePoint> routePoints = new List<RoutePoint>();
             var trackPoints = track.Element(ns + "trkseg").Elements(ns + "trkpt");
             foreach (XElement trackPoint in trackPoints)
---
+++ CycleCycleCycle/Services/RouteCreators/RouteCreatorException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CycleCycleCycle.Services.RouteCreators
+{
+    public class RouteCreatorException : Exception
+    {
+        public RouteCreatorException() : base() { }
+        public RouteCreatorException(string message) : base(message) { }
+        public RouteCreatorException(string message, Exception exception) : base(message, exception) { }
+    }
+}
